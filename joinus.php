4405c64dcf41ca6c4a3b11a84eeeb08d69e01ea9
Shoshana Swell <shoshanaswell@dhcp-rhodes-3310.eduroam.cornell.edu>
2018-04-19 14:09:47 -0400

    submit

--- joinus.php
+++ joinus.php
@@ -5,9 +5,60 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+  <title>Join Us</title>
 
 </head>
 <body>
-</body>
+  <?php include("includes/header.php");?>
+  <h1>Join Us </h1>
+  <img class="banner" alt="banner" src="images/joinus1.jpg"/>
+  <div>
+    <p> The research, invention, and design project courses specific to AguaClara are
+      focused on developing technology that can be used to improve the drinking water
+      quality of surface water sources in the Global South. The program is structured
+      to allow students with different levels of expertise to collaborate, invent, and
+      change the world.
+    </p>
+  </div>
 
-</html>
+<div>
+<h2> Why should you join our mission?</h2>
+
+<p> “Aguaclara has made it clear to me the importance of communication
+ and lifelong friendships; that the key to engineering is a foundation
+of understanding across ideologies, languages, and cultures. Rather
+than becoming overwhelmed by the daunting mission that defines Aguaclara,
+being a part of the team has shown me that there is sophistication in simplicity,
+innovation in friendship, and beauty in engineering."
+</p>
+<p> - Christopher Galantino '19 </p>
+<img class="testimonial_image" alt="christopher" src="images/chris1.jpg"/>
+
+</div>
+
+<div>
+  <h2> Courses </h2>
+  <p>Joining Aguaclara as an undergraduate project
+    team member requires you to take CEE 2550 to
+    gain credit for the research conducted.
+  </p>
+    <ul>
+      <li>CEE 2550: AguaClara: Sus H2O Supply Proj</li>
+      <li>CEE 4540: Sustainable Municipal Drinking Water Treatment</li>
+      <li>CEE 4550 [GRADUATE: CEE 5051-5052]</li>
+      <li>CEE 3090 </li>
+      <li>International Student Research Internships</li>
+    </ul>
+</div>
+
+<div>
+<h2> Recruitment </h2>
+<p> Information Sessions </p>
+<p> We hold information sessions for new members in the beginning
+ of the every semester. </p>
+<p> 1/24 Wednesday 2:55-4:10 Hollister 366 </p>
+<p> 1/25 Thursday: 2:55 - 4:10 Hollister 366 </p>
+</div>
+    </body>
+
+    </html>
