2216803d42aa4e9724229237890640f7203bb6cc
Shoshana Swell <shoshanaswell@dhcp-hol-154.eduroam.cornell.edu>
2018-04-24 15:26:02 -0400

    submit

--- joinus.php
+++ joinus.php
@@ -21,69 +21,67 @@
     </p>
   </div>
 
-<div>
-<h2> Why should you join our mission?</h2>
+  <div>
+    <h2> Why should you join our mission?</h2>
 
-<p> “Aguaclara has made it clear to me the importance of communication
- and lifelong friendships; that the key to engineering is a foundation
-of understanding across ideologies, languages, and cultures. Rather
-than becoming overwhelmed by the daunting mission that defines Aguaclara,
-being a part of the team has shown me that there is sophistication in simplicity,
-innovation in friendship, and beauty in engineering."
-</p>
-<p> - Christopher Galantino '19 </p>
-<div>
-<img class="testimonial_image" alt="christopher" src="images/chris1.jpg"/>
-</div>
-</div>
+    <p> “Aguaclara has made it clear to me the importance of communication
+      and lifelong friendships; that the key to engineering is a foundation
+      of understanding across ideologies, languages, and cultures. Rather
+      than becoming overwhelmed by the daunting mission that defines Aguaclara,
+      being a part of the team has shown me that there is sophistication in simplicity,
+      innovation in friendship, and beauty in engineering."
+    </p>
+    <p> - Christopher Galantino '19 </p>
+    <div>
+      <img class="testimonial_image" alt="christopher" src="images/chris1.jpg"/>
+    </div>
+  </div>
 
-<div>
-<div>
-<p> “Aguaclara has made it clear to me the importance of communication
- and lifelong friendships; that the key to engineering is a foundation
-of understanding across ideologies, languages, and cultures. Rather
-than becoming overwhelmed by the daunting mission that defines Aguaclara,
-being a part of the team has shown me that there is sophistication in simplicity,
-innovation in friendship, and beauty in engineering."
-
-</p>
-</div>
-<p> “Aguaclara has made it clear to me the importance of communication
- and lifelong friendships; that the key to engineering is a foundation
-of understanding across ideologies, languages, and cultures. Rather
-than becoming overwhelmed by the daunting mission that defines Aguaclara,
-being a part of the team has shown me that there is sophistication in simplicity,
-innovation in friendship, and beauty in engineering."
-</p>
-<p> - Clare O'Connor '19 </p>
-<div>
-<img class="testimonial_image" alt="christopher" src="images/clare1.jpg"/>
-</div>
-</div>
-<div>
-  <h2> Courses </h2>
-  <p>Joining Aguaclara as an undergraduate project
-    team member requires you to take CEE 2550 to
-    gain credit for the research conducted.
-  </p>
+  <div>
+    <div>
+      <p> “Being surrounded by so many people who are equally committed to using
+        engineering to benefit communities is incredible. It has been so special to
+        do research, watch the implementation of the team’s hard work, and have the
+        opportunity to visit plants in Honduras. AguaClara has inspired a change in
+        my career path to focus more on safe water."
+      </p>
+    </div>
+    <p> “Aguaclara has made it clear to me the importance of communication
+      and lifelong friendships; that the key to engineering is a foundation
+      of understanding across ideologies, languages, and cultures. Rather
+      than becoming overwhelmed by the daunting mission that defines Aguaclara,
+      being a part of the team has shown me that there is sophistication in simplicity,
+      innovation in friendship, and beauty in engineering."
+    </p>
+    <p> - Clare O'Connor '19 </p>
+    <div>
+      <img class="testimonial_image" alt="christopher" src="images/clare1.jpg"/>
+    </div>
+  </div>
+  <div>
+    <h2> Courses </h2>
+    <p>Joining Aguaclara as an undergraduate project
+      team member requires you to take CEE 2550 to
+      gain credit for the research conducted.
+    </p>
     <ul>
       <li>CEE 2550: AguaClara: Sustainable Water Supply Project</li>
       <li>CEE 4540: Sustainable Municipal Drinking Water Treatment</li>
       <li>CEE 4550: AguaClara: Sustainable Water Supply Project [GRADUATE: CEE 5051-5052]</li>
       <li>CEE 3090: Special Topics in Civil and Environmental
-Engineering</li>
-      <li>International Student Research Internships</li>
-    </ul>
-</div>
+        Engineering</li>
+        <li>International Student Research Internships</li>
+      </ul>
+    </div>
 
-<div>
-<h2> Recruitment </h2>
-<p> Information Sessions </p>
-<p> We hold information sessions for new members in the beginning
- of the every semester. </p>
-<p> 1/24 Wednesday 2:55-4:10 Hollister 366 </p>
-<p> 1/25 Thursday: 2:55 - 4:10 Hollister 366 </p>
-</div>
+    <div>
+      <h2> Recruitment </h2>
+      <p> Information Sessions </p>
+      <p> We hold information sessions for new members in the beginning
+        of the every semester. </p>
+        <p> 1/24 Wednesday 2:55-4:10 Hollister 366 </p>
+        <p> 1/25 Thursday: 2:55 - 4:10 Hollister 366 </p>
+      </div>
     </body>
 
     </html>
