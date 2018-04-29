8a47152bd0b7555dee22505498941e4fba12c534
Adrianne Han <Adji@dhcp-gs-4883.eduroam.cornell.edu>
2018-04-28 20:25:59 -0400

    fabrication subteam page

--- fabrication.php
+++ fabrication.php
@@ -17,5 +17,92 @@
   <!--Nav Bar-->
    <?php include("includes/nav.php");?>
 
+<h1 id="fabrication_title"> Fabrication Subteams </h1>
+
+<h2 class="blue">Horizontal Filtration</h2>
+<div class= "fabrication_boarder"> </div>
+<div class="fabrication_team_container">
+  <div id="fabrication_team_1_txt">
+    <p>Horizontal filtration is a completely new innovation for the AguaClara team, arising
+      from a desire to make the 1 L/s plants easier to make and ship by eliminating the
+      Enclosed Stacked Rapid Sand Filter. To create a horizontal filter, aspects of the
+      sedimentation tanks and current filter design will be used, such as the relationship between
+      backwash and operational speed, as well as the design of the plate settlers. Our goals for the
+      semester are to begin design and fabrication of a horizontal flow filter. This will involve characterization
+      of key parameters, using Fusion360 to make a model of the system, and fabrication of a prototype. </p>
+    <h3> Members: </h3>
+    <ul>
+      <li>Clare O'Connor | ceo45@cornell.edu </li>
+      <li>Chris Galantino | crg226@cornell.edu </li>
+      <li>Corson Chao | cac468@cornell.edu </li>
+    </ul>
+  </div>
+  <img src="images/horizontal_filtration.jpg" id="fabrication_team_1_image" alt = "Horizontal Filtration">
+</div>
+
+<h2 class="green">Sensor Development </h2>
+<div class= "fabrication_boarder_2"> </div>
+<div class="fabrication_team_container">
+  <div id="fabrication_team_2_txt">
+    <p>The goal of the Sensor Development subteam is to create sensors that help monitor water quality
+       during the water treatment process, for the purpose of improving experimentation in other subteams
+       and facilitating the work of plant operators. In particular, the team has been working on a particle
+       concentration and turbidity sensor for the last few semesters, beginning with a fluidized bed solids
+       detector developed for in-lab use and now a submersible solids detector for field use.
+        The submersible solids detector is the main focus for the team this semester.
+        Two different designs based on an endoscope and a pre-developed turbidity sensor will be tested
+        to measure the height of the sludge blanket in the sedimentation tanks.<p>
+    <h3> Members: </h3>
+    <ul>
+      <li>Lois Lee | LL556@cornell.edu </li>
+      <li>Srilekha Vangavolu | SV397@cornell.edu </li>
+      <li> Hannah Si | HS649@cornell.edu</li>
+    </ul>
+  </div>
+  <img src="images/horizontal_filtration.jpg" id="fabrication_team_2_image" alt = "Sensor Development">
+</div>
+
+<h2 class="blue">High Flow Float Valve </h2>
+<div class= "fabrication_boarder"> </div>
+<div class="fabrication_team_container">
+  <div id="fabrication_team_3_txt">
+    <p> High Flow Float Valve is a fabrication team that will fabricate a float valve to
+      alleviate distribution tank overflow during off-peak hours. The high flow float valve will control
+      the flow rate of water into the distribution tank. The distribution tank is the storage tank for clean
+      water exiting the treatment plant before it is distributed to the community, and overflows when the tank
+       is full. Overflow happens mainly during off-peak hours from midnight to dawn when demand is low.
+       Avoiding overflow is desirable because the excess treated drinking water is wasted.
+       To avoid the tank overflowing, the float valve will decrease the flow rate as the tank fills up.</p>
+   <h3> Members: </h3>
+   <ul>
+     <li>Felix Yang | fyy2@cornell.edu  </li>
+     <li>Julia Timko | jt663@cornell.edu  </li>
+     <li>Alycia Storch | ats228@cornell.edu </li>
+
+   </ul>
+
+  </div>
+  <img src="images/ram_pump.jpg" id="fabrication_team_3_image" alt = "High Flow Float Valve">
+</div>
+
+<h2 class="green">Ram Pump</h2>
+<div class= "fabrication_boarder_2"> </div>
+<div class="fabrication_team_container">
+  <div id="fabrication_team_4_txt">
+    <p> The purpose of the Ram Pump team is to design and develop a properly functioning hydraulic ram pump,
+      or hydram, for implementation in AguaClara plants. The hydram can be used to deliver water from below
+       the facility back to the top for utilization in chemical stock tanks or to collect water at higher
+       elevations for alternative uses.<p>
+     <h3> Members: </h3>
+     <ul>
+       <li>Maile McCann | mm2498@cornell.edu </li>
+       <li>Steven Lopez | sl2297@cornell.edu </li>
+       <li>Will Lopez | wml43@cornell.edu </li>
+
+     </ul>
+  </div>
+  <img src="images/ram_pump.jpg" id="fabrication_team_4_image" alt = "Ram Pump">
+</div>
+
 </body>
 </html>
