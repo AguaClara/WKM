0c7a1210d9e66f7e0e0b4f66b117bf4759459ca3
Adrianne Han <Adji@Adriannes-MacBook-Pro.local>
2018-04-29 22:10:41 -0400

    adrianne 10:10

--- fabrication.php
+++ fabrication.php
@@ -30,6 +30,9 @@
       backwash and operational speed, as well as the design of the plate settlers. Our goals for the
       semester are to begin design and fabrication of a horizontal flow filter. This will involve characterization
       of key parameters, using Fusion360 to make a model of the system, and fabrication of a prototype. </p>
+    </div>
+    <img src="images/horizontal_filtration.jpg" id="fabrication_team_1_image" alt = "Horizontal Filtration">
+    <div class="fabrication_team_members">
     <h3> Members: </h3>
     <ul>
       <li>Clare O'Connor | ceo45@cornell.edu </li>
@@ -37,7 +40,6 @@
       <li>Corson Chao | cac468@cornell.edu </li>
     </ul>
   </div>
-  <img src="images/horizontal_filtration.jpg" id="fabrication_team_1_image" alt = "Horizontal Filtration">
 </div>
 
 <h2 class="green">Sensor Development </h2>
@@ -52,14 +54,16 @@
         The submersible solids detector is the main focus for the team this semester.
         Two different designs based on an endoscope and a pre-developed turbidity sensor will be tested
         to measure the height of the sludge blanket in the sedimentation tanks.<p>
-    <h3> Members: </h3>
-    <ul>
-      <li>Lois Lee | LL556@cornell.edu </li>
-      <li>Srilekha Vangavolu | SV397@cornell.edu </li>
-      <li> Hannah Si | HS649@cornell.edu</li>
-    </ul>
-  </div>
+    </div>
   <img src="images/horizontal_filtration.jpg" id="fabrication_team_2_image" alt = "Sensor Development">
+  <div class="fabrication_team_members">
+  <h3> Members: </h3>
+  <ul>
+    <li>Lois Lee | LL556@cornell.edu </li>
+    <li>Srilekha Vangavolu | SV397@cornell.edu </li>
+    <li> Hannah Si | HS649@cornell.edu</li>
+  </ul>
+</div>
 </div>
 
 <h2 class="blue">High Flow Float Valve </h2>
@@ -73,6 +77,8 @@
        is full. Overflow happens mainly during off-peak hours from midnight to dawn when demand is low.
        Avoiding overflow is desirable because the excess treated drinking water is wasted.
        To avoid the tank overflowing, the float valve will decrease the flow rate as the tank fills up.</p>
+     </div>
+     <div class="fabrication_team_members">
    <h3> Members: </h3>
    <ul>
      <li>Felix Yang | fyy2@cornell.edu  </li>
@@ -93,6 +99,9 @@
       or hydram, for implementation in AguaClara plants. The hydram can be used to deliver water from below
        the facility back to the top for utilization in chemical stock tanks or to collect water at higher
        elevations for alternative uses.<p>
+
+       </div>
+ <div class="fabrication_team_members">
      <h3> Members: </h3>
      <ul>
        <li>Maile McCann | mm2498@cornell.edu </li>
