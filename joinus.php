9c5f07ae3f9b0c0e78728ae91fb815c09212793e
Shoshana Swell <shoshanaswell@Shoshanas-MacBook-Pro.local>
2018-04-29 20:27:30 -0400

    sho-8.27

--- joinus.php
+++ joinus.php
@@ -2,10 +2,16 @@
 <html>
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> AguaClara Cornell </title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+  integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
   <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
-  <title>Join Us</title>
 
 </head>
 <body>
@@ -25,6 +31,7 @@
     <h2> Why should you join our mission?</h2>
     <div class="joinus_container_testimonial">
       <div class = "joinus_testimonial1">
+<div class="joinus_testimonial_paragraph">
         <p> “Aguaclara has made it clear to me the importance of communication
           and lifelong friendships; that the key to engineering is a foundation
           of understanding across ideologies, languages, and cultures. Rather
@@ -32,7 +39,8 @@
           being a part of the team has shown me that there is sophistication in simplicity,
           innovation in friendship, and beauty in engineering."
         </p>
-        <div class = "joinus_testimonial1_image">
+</div>
+        <div class = "joinus_testimonial_image">
           <div>
             <img class="joinus_testimonial_image" alt="christopher" src="images/chris1.jpg"/>
           </div>
@@ -42,33 +50,36 @@
     </div>
 
     <div>
+      <div class = "joinus_testimonial1">
+
       <div>
+        <div class="joinus_testimonial_paragraph">
+
         <p> “Being surrounded by so many people who are equally committed to using
           engineering to benefit communities is incredible. It has been so special to
           do research, watch the implementation of the team’s hard work, and have the
           opportunity to visit plants in Honduras. AguaClara has inspired a change in
           my career path to focus more on safe water."
         </p>
+</div>
       </div>
-      <p> “Aguaclara has made it clear to me the importance of communication
-        and lifelong friendships; that the key to engineering is a foundation
-        of understanding across ideologies, languages, and cultures. Rather
-        than becoming overwhelmed by the daunting mission that defines Aguaclara,
-        being a part of the team has shown me that there is sophistication in simplicity,
-        innovation in friendship, and beauty in engineering."
-      </p>
-      <p> - Clare O'Connor, Environmental Engineering '18 </p>
+
+      <div class = "joinus_testimonial_image">
       <div>
-        <img class="testimonial_image" alt="clare" src="images/clare1.jpg"/>
+        <img class="testimonial_image" alt="clare" src="images/clare1.png"/>
       </div>
+      <p> - Clare O'Connor, Environmental Engineering '18 </p>
+</div>
+
+
     </div>
   </div>
 
 
   <div>
-    <h2> Courses </h2>
+    <h2> Courses  </h2>
     <p>Joining Aguaclara as an undergraduate project
-      team member requires you to take CEE 2550 to
+      team member requires you to take <p class= "joinus_bold">CEE 2550</p> to
       gain credit for the research conducted.
     </p>
     <ul>
