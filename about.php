e3823953b1e03a5000b032cda3c1e5ebcded2853
Adrianne Han <Adji@dhcp-hol-2186.eduroam.cornell.edu>
2018-05-08 15:15:36 -0400

    adrianne 3:15pm

--- about.php
+++ about.php
@@ -10,8 +10,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <script src="js/navbar_scroll.js"></script>
-  <script src="js/navbar_active.js"></script>
+
 
   <title>About</title>
 
@@ -108,7 +107,8 @@
 
 
 
-
+  <script src="js/navbar_scroll.js"></script>
+  <script src="js/navbar_active.js"></script>
 
 
 </body>
