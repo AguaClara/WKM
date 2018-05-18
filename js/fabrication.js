a1473b2713d34eecd7c06bd4ed583291648f5d48
Adrianne Han <Adji@Adriannes-MacBook-Pro.local>
2018-05-18 02:56:09 -0400

    Adrianne 3am

--- js/fabrication.js
+++ js/fabrication.js
@@ -1,15 +1,23 @@
 
 $(document).ready(function(){
   $("#fabrication_team1").click(function(){
-    $("#fabrication_team_container1").toggle(1000);
+    $("#fabrication_team_container1").animate({
+            height: 'toggle'
+        });
   })
   $("#fabrication_team2").click(function(){
-    $("#fabrication_team_container2").toggle(1000);
+    $("#fabrication_team_container2").animate({
+            height: 'toggle'
+        });
   })
   $("#fabrication_team3").click(function(){
-    $("#fabrication_team_container3").toggle(1000);
-  })
+    $("#fabrication_team_container3").animate({
+            height: 'toggle'
+        });
+    })
   $("#fabrication_team4").click(function(){
-    $("#fabrication_team_container4").toggle(1000);
-  })
+    $("#fabrication_team_container4").animate({
+            height: 'toggle'
+        });
+    })
 });
