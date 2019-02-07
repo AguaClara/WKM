26f29e307547b8fc7cc3cb3cf25d90f75f16efa7
Shirley Chen <sc2552@cornell.edu>
2019-02-06 20:52:28 -0500

    updated subteam information

--- js/AA.js
+++ js/AA.js
@@ -1,18 +1,23 @@
 
-$(document).ready(function(){
-  $("#AA_team1").click(function(){
+$(document).ready(function () {
+  $("#AA_team1").click(function () {
     $("#AA_team_container1").animate({
-            height: 'toggle'
-        });
+      height: 'toggle'
+    });
   })
-  $("#AA_team2").click(function(){
+  $("#AA_team2").click(function () {
     $("#AA_team_container2").animate({
-            height: 'toggle'
-        });
+      height: 'toggle'
+    });
   })
-  $("#AA_team3").click(function(){
+  $("#AA_team3").click(function () {
     $("#AA_team_container3").animate({
-            height: 'toggle'
-        });
-})
+      height: 'toggle'
+    });
+  })
+  $("#AA_team4").click(function () {
+    $("#AA_team_container4").animate({
+      height: 'toggle'
+    });
+  })
 });
